4dc0658b2d1af05bc725360dd8ce11299da039d0
ho-229 <56023510+ho-229@users.noreply.github.com>
2023-04-18 13:56:07 +0800

    Update auth_test.exs

--- test/cetacea_web/controllers/auth_test.exs
+++ test/cetacea_web/controllers/auth_test.exs
@@ -1,52 +1,54 @@
 defmodule CetaceaWeb.ErrorHTMLTest do
   use CetaceaWeb.ConnCase, async: true
 
-  test "Pubkey Login V1" do
-    body =
-      build_conn()
-      |> post(~p"/api/auth/pubkey_login_v1")
-      |> json_response(200)
+  defp api_auth(path, body \\ nil) do
+    build_conn()
+    |> post(path, body)
+    |> json_response(200)
+  end
 
+  test "Invalid Pubkey Login V1" do
+    body = api_auth(~p"/api/auth/pubkey_login_v1")
     assert body["error_code"] == "InvalidPubkey"
     assert body["error_message"] == "pubkey is not exist"
 
-    body =
-      build_conn()
-      |> post(~p"/api/auth/pubkey_login_v1", %{pubkey: "fuck lemonhx", sign_jwt_duration: -1})
-      |> json_response(200)
+    gen_invalid_pubkey = fn ->
+      len = :rand.uniform(1000)
+      :crypto.strong_rand_bytes(if len == 255, do: len + 1, else: len)
+    end
 
-    assert body["jwt_token"]
+    for a <- Enum.shuffle([nil, gen_invalid_pubkey.(), :rand.uniform() * 10000]) do
+      for b <- Enum.shuffle([nil, gen_invalid_pubkey.(), trunc(:rand.uniform() * 10000)]) do
+        body = api_auth(~p"/api/auth/pubkey_login_v1", %{pubkey: a, sign_jwt_duration: b})
+        assert body["error_code"] == "InvalidPubkey", "expect error occurred"
+      end
+    end
   end
 
-  test "JWT Login V1" do
-    body =
-      build_conn()
-      |> post(~p"/api/auth/jwt_login_v1")
-      |> json_response(200)
-
+  test "Invalid JWT Login V1" do
+    body = api_auth(~p"/api/auth/jwt_login_v1")
     assert body["error_code"] == "InvalidJWTToken"
     assert body["error_message"] == "jwt token is not exist"
 
-    body =
-      build_conn()
-      |> post(~p"/api/auth/jwt_login_v1", %{jwt_token: "fuck lemonhx"})
-      |> json_response(200)
-
-    assert body["error_code"] == "InvalidJWTToken"
-    assert body["error_message"] == "jwt token is invalid: signature_error"
+    for a <-
+          Enum.shuffle([
+            nil,
+            :crypto.strong_rand_bytes(:rand.uniform(1000)),
+            :rand.uniform() * 10000
+          ]) do
+      body = api_auth(~p"/api/auth/jwt_login_v1", %{jwt_token: a})
+      assert body["error_code"] == "InvalidJWTToken"
+    end
   end
 
-  test "Login" do
+  test "Login V1" do
     jwt_token_body =
-      build_conn()
-      |> post(~p"/api/auth/pubkey_login_v1", %{pubkey: "fuck lemonhx", sign_jwt_duration: -1})
-      |> json_response(200)
+      api_auth(~p"/api/auth/pubkey_login_v1", %{
+        pubkey: :crypto.strong_rand_bytes(255),
+        sign_jwt_duration: -1
+      })
 
-    body =
-      build_conn()
-      |> post(~p"/api/auth/jwt_login_v1", jwt_token_body)
-      |> json_response(200)
-
+    body = api_auth(~p"/api/auth/jwt_login_v1", jwt_token_body)
     assert Enum.empty?(body)
   end
 end
