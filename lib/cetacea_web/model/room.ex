5701c6cad8f949feddb7361d06de312fe0db6026
imlyzh <enterindex@gmail.com>
2023-04-20 16:22:47 +0800

    added test room table

--- lib/cetacea_web/model/room.ex
+++ lib/cetacea_web/model/room.ex
@@ -1,4 +1,23 @@
+defmodule TestRoom do
+  use Ecto.Schema
 
+  schema "test_room" do
+    field :room_id, :id, primary_key: true
+    field :room_version, :string
+    field :name, :string
+    field :description, :string
+    field :avatar, :string
+    # field :join_rule, RoomJoinRuleV1
+    # field :join_restriction, Vec<RoomJoinRestrictionV1>
+    field :created_at, :utc_datetime
+    embeds_many :members, UserHeaderV1
+    embeds_many :admins, UserHeaderV1
+    embeds_many :banned_members, UserHeaderV1
+    # field :default_admin_abilities, Vec<AdminAbilitiesV1>
+    # field :default_member_abilities, Vec<AdminAbilitiesV1>
+    embeds_many :messages, MessageV1
+  end
+end
 
 defmodule MessageV1 do
   use Ecto.Schema
